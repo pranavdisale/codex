5e8892c4219ad548ee668d6799e9d92214f8272e
pranavdisale <pranavdisale50@gmail.com>
2024-07-06 13:23:02 +0530

    practial 1 assignment 1 added

---
+++ practical1/lib/Assignment1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Assignment1 extends StatelessWidget {
+  const Assignment1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Demo App'),
+        backgroundColor: Colors.orange[400],
+      ),
+    );
+  }
+}
